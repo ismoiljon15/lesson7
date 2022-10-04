92f560c41eff0ae3e2de745fbe6ef6dc68ff4f35
ismoiljon15 <114994170+ismoiljon15@users.noreply.github.com>
2022-10-04 19:35:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,113 @@
+console.log("JS is started");
+
+
+// Compare operators 
+
+
+
+console.log( 5 > 3 ); // true
+console.log( 5 < 3 ); // false
+console.log( 7 == 7 ); // true
+console.log( 12 == 25 ); // false
+console.log( 3 != 6  ); // true
+
+// == - faqat qiymatni tekshiradi
+// === - oldin type keyin qiymatni tekshiradi
+// console.log( 5 === 5 );
+
+// console.log( "salom" === "Salom" );
+
+
+// let result = 5 === 6 
+
+// console.log("result>>>", result);
+
+// if ( result ){
+//     console.log("Ha");
+// }else{
+//     console.log("yoq");
+// }
+
+
+
+// % - qoldiqni topadi
+// console.log( 10 % 5 ); // 0
+// console.log( 10 % 3 ); // 1
+// console.log( 25 % 35 ); // 25
+
+// let son = 12
+
+// if( son % 2 === 0 ){
+// console.log("Juft son");
+// }else{
+//     console.log("Toq son");
+// }
+
+
+
+// let yosh = 15
+
+// if( yosh >= 18 ){
+//     console.log("Katta yosh");
+// }else{
+//     console.log("Kichik yosh");
+// }
+
+
+// let pul = 380
+
+// if(pul >= 500){
+//     console.log("iPhone 11 Pro Max");
+// }else if(pul >= 300){
+//     console.log("Galaxy S20");
+// }else if(pul >= 100){
+//     console.log("Red mi");
+// }else{
+//     console.log("Nokia Ishlatavering!");}
+
+
+
+let son = 21
+
+if(son += 1){
+    console.log("bir xonali son");
+}
+
+if(son -= 2){
+    console.log("ikki xonali son");
+}
+
+
+let raqam = 25
+if( raqam % 3){
+    console.log("Fizz");
+}else{
+    console.log("qoldiqli");
+}
+if( raqam %= 5){
+    console.log("Buzz");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
